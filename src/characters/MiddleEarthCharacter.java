5aa36d684216d180d6c3125b2babeffb19db58b9
Borui Zhang <borui@botenders.com>
2025-02-26 18:44:33 -0600

    Javadoc for MiddleEarthCharacter, created the abstract methods attack and getRace as well as adding their comments

--- src/characters/MiddleEarthCharacter.java
+++ src/characters/MiddleEarthCharacter.java
@@ -6,12 +6,51 @@ public abstract class MiddleEarthCharacter
 	private double health;
 	private double power;
 	
+	
+	
+	/** Constructor for MiddleEarthCharacter
+	 * @param name is the name of the character
+	 * @param health indicates character HP 
+	 * @param power indicates damage points the character inflicts
+	 */
 	public MiddleEarthCharacter(String name, double health, double power) {
 		super();
 		this.name = name;
 		this.health = health;
 		this.power = power;
 	}
+	
+	/** This is an abstract method for when a character attacks another character
+	 * @param target is the character being attacked
+	 * @return true if the attack is successful and reduced target’s health, 
+	 * 		   false if the attack was ineffective, or attack was against their own kin.
+	 */
+	public abstract Boolean attack(MiddleEarthCharacter target);
+	
+	/**This is an abstract method to get the race of a character
+	 * @return returns the race name of the character as a string 
+	 */
+	public abstract String getRace();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public String getName() {
 		return name;
 	}
