d62fc9d5a5d9d230e0c6c48744610994ee77849f
Borui Zhang <borui@botenders.com>
2025-02-26 19:04:12 -0600

    Javadoc for the rest of the getters and setters, as well as displayInfo

--- src/characters/MiddleEarthCharacter.java
+++ src/characters/MiddleEarthCharacter.java
@@ -32,12 +32,15 @@ public abstract class MiddleEarthCharacter
 	 */
 	public abstract String getRace();
 	
+	/** prints out the Name, Health and power of a character 
+	 * 
+	 */
 	public void displayInfo() {
 		System.out.println(this.toString());
 	}
 	
 
-	/** toString method displays name health and power, used in displayInfo 
+	/** toString method returns name health and power, used in displayInfo 
 	 *
 	 */
 	@Override
@@ -51,21 +54,38 @@ public abstract class MiddleEarthCharacter
 	public String getName() {
 		return name;
 	}
+	
 	/** setter for name
 	 * @param name of character
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/** Getter for health
+	 * @return health
+	 */
 	public double getHealth() {
 		return health;
 	}
+	
+	/** Setter for health
+	 * @param health
+	 */
 	public void setHealth(double health) {
 		this.health = health;
 	}
+	
+	/** Getter for power
+	 * @return power
+	 */
 	public double getPower() {
 		return power;
 	}
+	
+	/** Setter for power
+	 * @param power
+	 */
 	public void setPower(double power) {
 		this.power = power;
 	}
