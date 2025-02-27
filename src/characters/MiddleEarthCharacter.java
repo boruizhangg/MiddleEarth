132ef6ec409b675cb25b100801155c083821b979
Borui Zhang <borui@botenders.com>
2025-02-26 18:32:40 -0600

    Created the fields for characters as well as their getters and setters

--- src/characters/MiddleEarthCharacter.java
+++ src/characters/MiddleEarthCharacter.java
@@ -1,5 +1,29 @@
 package characters;
 
-public class MiddleEarthCharacter {
+public abstract class MiddleEarthCharacter 
+{
+	private String name;
+	private double health;
+	private double power;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getHealth() {
+		return health;
+	}
+	public void setHealth(double health) {
+		this.health = health;
+	}
+	public double getPower() {
+		return power;
+	}
+	public void setPower(double power) {
+		this.power = power;
+	}
+	
 
 }
