2841cd5bfc730545ad7b89d71346386cc45a033c
Hao Ouyang <121321758+joe520088@users.noreply.github.com>
2025-02-27 22:39:52 -0600

    initial thoughts on attack for human

--- src/characters/Human.java
+++ src/characters/Human.java
@@ -2,21 +2,38 @@ package characters;
 
 public class Human extends MiddleEarthCharacter{
 	
+	public double totalDamage; 
 	
-	public Human(String name, double health, double power) {
+	public Human(String name, double health, double power) 
+	{
 		super(name, health, power);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public Boolean attack(MiddleEarthCharacter target) {
-		// TODO Auto-generated method stub
+	public Boolean attack(MiddleEarthCharacter target) 
+	{
+		if(target.getRace().equals("Wizard") || target.getRace().equals("wizard") )
+		{
+			System.out.println("1.5 x damage against Wizard");
+			totalDamage = this.getPower() * 1.5;
+		
+		}
+		else if(target.getRace() == "Orc" || target.getRace() == "orc")
+		{
+			System.out.println("Ineffective (0 damage) against Orc");
+		}
+		if(target.getRace() == "Human" || target.getRace() == "human")
+		{
+			System.out.println("Ineffective (0 damage) against Human");
+		}
 		return null;
 	}
 
 	@Override
-	public String getRace() {
-		// TODO Auto-generated method stub
+	public String getRace() 
+	{
+		
 		return null;
 	}
 
