2fc6634f0aaa6982b7f2bbb1cf141d16cedfaa50
Hao Ouyang <121321758+joe520088@users.noreply.github.com>
2025-03-01 16:28:08 -0600

    initial final design for human sub class declare base damage and total damage to minus the health of target

--- src/characters/Human.java
+++ src/characters/Human.java
@@ -3,38 +3,38 @@ package characters;
 public class Human extends MiddleEarthCharacter{
 	// declare damage
 	public double totalDamage; 
+	public double baseDamage = 2;
 	
 	public Human(String name, double health, double power) 
 	{
 		super(name, health, power);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public Boolean attack(MiddleEarthCharacter target) 
 	{
+		double targetHealth = target.getHealth();
 		if(target.getRace().equals("Wizard") || target.getRace().equals("wizard") )
 		{
-			System.out.println("1.5 x damage against Wizard");
-			totalDamage = this.getPower() * 1.5;
-		
+			totalDamage = baseDamage * 1.5;
+			targetHealth = targetHealth - totalDamage;
+			return true;
 		}
-		else if(target.getRace() == "Orc" || target.getRace() == "orc")
+		else if(target.getRace().equals( "Orc") || target.getRace() == "orc")
 		{
-			System.out.println("Ineffective (0 damage) against Orc");
+			return false;
 		}
-		if(target.getRace() == "Human" || target.getRace() == "human")
+		else if(target.getRace().equals("Human") || target.getRace().equals("human"))
 		{
-			System.out.println("Ineffective (0 damage) against Human");
+			return false;
 		}
-		return null;
+		return true;
 	}
 
 	@Override
 	public String getRace() 
 	{
-		
-		return null;
+		return "Human";
 	}
 
 	
