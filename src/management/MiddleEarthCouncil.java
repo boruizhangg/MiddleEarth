d484643fe56b2fec52192410191381525b125bc3
eteva <etevans2327@gmail.com>
2025-03-03 16:45:01 -0600

    Implements getInstance method for MiddleEarthCouncil(Singleton)

--- src/management/MiddleEarthCouncil.java
+++ src/management/MiddleEarthCouncil.java
@@ -1,5 +1,20 @@
 package management;
+import characters.MiddleEarthCharacter;
 
 public class MiddleEarthCouncil {
-
+	
+	private static MiddleEarthCouncil instance;
+	private CharacterManager characterManager;
+	
+	private MiddleEarthCouncil() {
+		characterManager = new CharacterManager();
+	}
+	
+	public static MiddleEarthCouncil getInsatnce() {
+		if(instance == null) {
+			instance = new MiddleEarthCouncil();
+		}
+		return instance;
+	}
+	
 }
