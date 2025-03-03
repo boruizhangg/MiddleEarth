c5807f0419d7812e78770011d6be731064424542
eteva <etevans2327@gmail.com>
2025-03-03 17:24:01 -0600

    Added Javadoc for MiddleEarthCouncil Class

--- src/management/MiddleEarthCouncil.java
+++ src/management/MiddleEarthCouncil.java
@@ -1,15 +1,25 @@
 package management;
-import characters.MiddleEarthCharacter;
 
 public class MiddleEarthCouncil {
 	
 	private static MiddleEarthCouncil instance;
 	private CharacterManager characterManager;
 	
+	/**
+	 * Private constructor to prevent direct instantiation.
+	 * Initializes the CharacterManager.
+	 */
 	private MiddleEarthCouncil() {
 		characterManager = new CharacterManager();
 	}
 	
+	/**
+	 * This method returns the single instance of MiddleEarthCouncil 
+	 * and ensures only one instance exists.
+	 * 
+	 * @return
+	 * 		The single instance of MiddleEarthCouncil
+	 */
 	public static MiddleEarthCouncil getInsatnce() {
 		if(instance == null) {
 			instance = new MiddleEarthCouncil();
@@ -17,6 +27,11 @@ public class MiddleEarthCouncil {
 		return instance;
 	}
 	
+	/**
+	 * This method provides access to the CharacterManager.
+	 * @return
+	 * 		The CharacterManager instance
+	 */
 	public CharacterManager getCharacterManager() {
 		return characterManager;
 	}
