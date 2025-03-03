e5790d863e42e30be73139f2594d26b089446d04
eteva <etevans2327@gmail.com>
2025-03-03 16:03:09 -0600

    Initial Elf subclass

---
+++ src/characters/Elf.java
@@ -0,0 +1,22 @@
+package characters;
+
+public class Elf extends MiddleEarthCharacter{
+
+	public Elf(String name, double health, double power) {
+		super(name, health, power);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Boolean attack(MiddleEarthCharacter target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
