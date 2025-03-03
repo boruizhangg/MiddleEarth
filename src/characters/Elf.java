dd3d1b4cb94f2f0f714c95b7baa6d6a70491162a
eteva <etevans2327@gmail.com>
2025-03-03 17:03:25 -0600

    Added Javadoc to Elf class

--- src/characters/Elf.java
+++ src/characters/Elf.java
@@ -2,10 +2,28 @@ package characters;
 
 public class Elf extends MiddleEarthCharacter{
 
+	/**
+	 * Constructor for Elf
+	 * @param name
+	 * 		Name of the Elf
+	 * @param health
+	 * 		Health points of the Elf
+	 * @param power
+	 * 		Attack Power of the Elf
+	 */
 	public Elf(String name, double health, double power) {
 		super(name, health, power);
 	}
-
+	
+	/**
+	 * Parameterized method that performs an attack on a target character.
+	 * Elves deal 1.5x damage to Orcs, normal damage to Humans and Wizards, 
+	 * and no damage to Elves or Dwarfs
+	 * @param target
+	 * 		The character being attacked
+	 * @return 
+	 * 		True if the attack is successful, false otherwise
+	 */	 
 	@Override
 	public Boolean attack(MiddleEarthCharacter target) {
 		if(target instanceof Elf || target instanceof Dwarf){
@@ -20,6 +38,9 @@ public class Elf extends MiddleEarthCharacter{
 		return true;
 	}
 
+	/**
+	 * Method that returns the race of the character
+	 */
 	@Override
 	public String getRace() {
 		return "Elf";
