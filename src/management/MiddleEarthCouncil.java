7b75b0729a749bca740f10fc84d185d94ffe958e
ethanevans33 <etevans2327@gmail.com>
2025-03-04 23:35:30 -0600

    Fixed Typo in method getInstance

--- src/management/MiddleEarthCouncil.java
+++ src/management/MiddleEarthCouncil.java
@@ -20,7 +20,7 @@ public class MiddleEarthCouncil {
 	 * @return
 	 * 		The single instance of MiddleEarthCouncil
 	 */
-	public static MiddleEarthCouncil getInsatnce() {
+	public static MiddleEarthCouncil getInstance() {
 		if(instance == null) {
 			instance = new MiddleEarthCouncil();
 		}
