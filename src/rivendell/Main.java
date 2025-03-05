b1fa97ecbbc788347d8ec5ba3b43e9e11f50fb47
Hao Ouyang <121321758+joe520088@users.noreply.github.com>
2025-03-04 23:50:09 -0600

    initial character adding for main

--- src/rivendell/Main.java
+++ src/rivendell/Main.java
@@ -12,9 +12,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		CharacterManager character = new CharacterManager();
-		Human human = new Human("Joe",1,1);
+		Human human = new Human("Jake",1,1);
+		Elf elf = new Elf("Jack", 1,1);
+		Orc orc = new Orc("Arc",1,1);
+		Dwarf dwarf = new Dwarf("dip",1,1);
+		Wizard wizard = new Wizard("W",1,1);
+		
 		
 		character.addCharacter(human);
+		character.addCharacter(elf);
+		character.addCharacter(orc);
+		character.addCharacter(dwarf);
+		character.addCharacter(wizard);
+		
+		character.getCharacter("Orc");
+		
+		
+
 	}
 
 }
